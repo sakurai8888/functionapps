fbdd025747544391f4faf814755d984167e72288
myglobaladm <myglobaladm@azdemomc.com>
2024-05-11 21:47:31 +0800

    commit add new file

---
+++ functionapp01/src/functions/httpTrigger02.js
@@ -0,0 +1,50 @@
+const { app } = require('@azure/functions');
+
+
+/*
+module.exports.MakeAPICall = async function (context,functioninput) {
+    // Perform the API call asynchronously
+    // ...
+    const myoutput = functioninput;
+
+    return `${myoutput}`;
+};
+*/
+
+
+
+
+app.http('httpTrigger02', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name01 = request.query.get('name01') || await request.text() || 'world';
+        const name02 = request.query.get('name02') || await request.text() || 'world';
+
+        const result = await myfunction(name01,name02);
+        //const result = name;
+        console.log(result)
+        return { body: `Hello how are you, ${result}!` };
+    }
+});
+
+
+
+
+
+async function myfunction(functioninput01,functioninput02) {
+    // Perform the API call asynchronously
+    // ...
+    const myoutput = functioninput01 +' ' + functioninput02;
+    await delay(3000); 
+    return `${myoutput}`;
+};
+
+
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
