65615dd5d5fcd322ad68a1ea4f566607f76ee46d
myglobaladm <myglobaladm@azdemomc.com>
2024-05-11 21:20:05 +0800

    commit and push

--- functionapp01/src/functions/httpTrigger01.js
+++ functionapp01/src/functions/httpTrigger01.js
@@ -1,13 +1,40 @@
 const { app } = require('@azure/functions');
 
+
+/*
+module.exports.MakeAPICall = async function (context,functioninput) {
+    // Perform the API call asynchronously
+    // ...
+    const myoutput = functioninput;
+
+    return `${myoutput}`;
+};
+*/
+
+async function myfunction(functioninput01,functioninput02) {
+    // Perform the API call asynchronously
+    // ...
+    const myoutput = functioninput01 +' ' + functioninput02;
+
+    return `${myoutput}`;
+};
+
 app.http('httpTrigger01', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name01 = request.query.get('name01') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        
+        const name02 = request.query.get('name02') || await request.text() || 'world';
+
+        const result = await myfunction(name01,name02);
+        //const result = name;
+        console.log(result)
+        return { body: `Hello how are you, ${result}!` };
     }
 });
+
+
