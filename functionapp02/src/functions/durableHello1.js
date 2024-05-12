61ba963e7b0399890015e36d14ea5123b2c89d6c
myglobaladm <myglobaladm@azdemomc.com>
2024-05-12 23:32:47 +0800

    commit new durable function

---
+++ functionapp02/src/functions/durableHello1.js
@@ -0,0 +1,33 @@
+const { app } = require('@azure/functions');
+const df = require('durable-functions');
+
+const activityName = 'durableHello1';
+
+df.app.orchestration('durableHello1Orchestrator', function* (context) {
+    const outputs = [];
+    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
+    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
+    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
+
+    return outputs;
+});
+
+df.app.activity(activityName, {
+    handler: (input) => {
+        return `Hello, ${input}`;
+    },
+});
+
+app.http('durableHello1HttpStart', {
+    route: 'orchestrators/{orchestratorName}',
+    extraInputs: [df.input.durableClient()],
+    handler: async (request, context) => {
+        const client = df.getClient(context);
+        const body = await request.text();
+        const instanceId = await client.startNew(request.params.orchestratorName, { input: body });
+
+        context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+        return client.createCheckStatusResponse(request, instanceId);
+    },
+});
