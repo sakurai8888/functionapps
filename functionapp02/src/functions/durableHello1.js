c5819d81003f3456d73a35047b3f1f23fef8df21
myglobaladm <myglobaladm@azdemomc.com>
2024-05-20 00:28:00 +0800

    commit changing

--- functionapp02/src/functions/durableHello1.js
+++ functionapp02/src/functions/durableHello1.js
@@ -45,6 +45,28 @@ df.app.orchestration("helloSequence", function* (context) {
 });
 
 
+df.app.orchestration("helloSequence02", function* (context) {
+  context.log("Starting chain sample");
+
+  //const output = [];
+  //const activity_maps = {"helloActivity01": "Tokyo", "helloActivity02": "long", "helloActivity01": "short"};
+  const activity_maps = ["short","adsf","1212"];
+  //const mapped = Object.entries(map).map(([k,v]) => `${k}_${v}`);
+  //const activityPromises = Object.entries(activity_maps).map(([k,v]) => context.df.callActivity(k, v));
+  const activityPromises = activity_maps.map((input) => context.df.callActivity("helloActivity01", input));
+  /*
+  output.push(yield context.df.callActivity("helloActivity01","Tokyo"));
+  output.push(yield context.df.callActivity("helloActivity02","long"));
+  output.push(yield context.df.callActivity("helloActivity01","short"));
+  */
+
+  
+  const output = yield Promise.all(activityPromises); // has errors
+  return output;
+});
+
+
+
 
 // Make an http trigger endpoint for the Orchestrator Function 
 // Endpoint url http://localhost:7071/runtime/webhooks/durabletask/orchestrators/helloSequence , post method
@@ -97,6 +119,32 @@ module.exports = async function (context, req) {
 
 
 
+module.exports = async function (context, req) {
+  const functionName = "helloSequence02"; // Replace with the name of your orchestrator function
+  const client = df.getClient(context);
+
+  try {
+    // Start the Durable Function orchestration
+    const instanceId = await client.startNew(functionName, undefined, req.body);
+
+    // Return the instanceId as the HTTP response
+    context.res = {
+      status: 202,
+      body: {
+        instanceId: instanceId,
+      },
+    };
+  } catch (error) {
+    // Return the error message as the HTTP response
+    context.res = {
+      status: 500,
+      body: {
+        error: "An error occurred while starting the orchestration.",
+        message: error.message,
+      },
+    };
+  }
+};
 
 
 
